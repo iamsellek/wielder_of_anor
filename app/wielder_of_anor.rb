1f4753f5e652e6b1111f3961f0e24f63bb88f94b
Chris Sellek <csellek@reverbnation.com>
2016-01-15 13:48:38 -0500

    Forgot to remove a comment. See? This is why this app exists.

--- app/wielder_of_anor.rb
+++ app/wielder_of_anor.rb
@@ -2,5 +2,4 @@ require_relative 'wielder_of_anor_helper'
 
 helper = WielderOfAnorHelper.new(ARGV[0], ARGV[1], Dir.pwd)
 
-# Run the git diff and export the results into docs/files_changed
 helper.search_for_forbidden_words
