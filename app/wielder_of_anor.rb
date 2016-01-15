940430a0690185a412565f2cc4d99bc11f5109a8
Chris Sellek <csellek@reverbnation.com>
2016-01-15 13:48:00 -0500

    Committing a file that wasn't saved for the last commit.

--- app/wielder_of_anor.rb
+++ app/wielder_of_anor.rb
@@ -3,5 +3,4 @@ require_relative 'wielder_of_anor_helper'
 helper = WielderOfAnorHelper.new(ARGV[0], ARGV[1], Dir.pwd)
 
 # Run the git diff and export the results into docs/files_changed
-helper.git_diff
 helper.search_for_forbidden_words
