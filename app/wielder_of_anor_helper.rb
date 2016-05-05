8c6bccf48d923fbb501df133db1c0acf3839abc8
Chris Sellek <chris.sellek@dudesolutions.com>
2016-05-05 13:24:24 -0400

    Getting rid of unnecessary '-a' in the git commit command. User will determine what files are committed by running git add command themselves.

--- app/wielder_of_anor_helper.rb
+++ app/wielder_of_anor_helper.rb
@@ -217,7 +217,7 @@ class WielderOfAnorHelper
             end
           end
         end
-        
+
         code_file.close
       end
     else
@@ -272,7 +272,7 @@ class WielderOfAnorHelper
     puts "\n\n"
 
     if input == "yes" || input == "y"
-      bash(%Q[git commit -a -m "#{@commit_message}"])
+      bash(%Q[git commit -m "#{@commit_message}"])
       puts "\n\n"
       puts "COMMITED."
       puts "\n\n"
